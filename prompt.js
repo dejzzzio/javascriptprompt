8a4182626f469fffa12a19b5a461e43944b76be6
dejzzzio <65173631+dejzzzio@users.noreply.github.com>
2021-02-01 08:32:48 +0100

    Create prompt.js
    tak

---
+++ prompt.js
@@ -0,0 +1,5 @@
+var nazwazmiennej = prompt("ile to 2+2") // tworzenie zmiennej + prompt z tekstem
+if (nazwazmiennej == "4") // jezeli wynik bedzie "4"
+ alert("Brawo umiesz liczyc") // powie ze umiesz liczyc
+else // jezeli dasz inne niz "4" zreferuje sie w dolny alert
+ alert("Brawo, nie umiesz liczyc!") // alert informuje ze wynik jest inny
